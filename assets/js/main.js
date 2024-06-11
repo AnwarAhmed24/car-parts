394cf3e154d53c4cf3a23c7ab330365f22e32b78
Anwar <aa182778@gmail.com>
2024-06-11 11:58:27 +0600

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,88 +6,10 @@ window.addEventListener('load', function () {
   })
 }); 
 
-$("#get-add-car").load("add-car.html #msform");
+$("#get-add-car").load("add-car.html #get-add-car");
+
 $(document).ready(function () {
 
-
-  // https://www.linkedin.com/in/atakangk/
-  //jQuery time
-  var current_fs, next_fs, previous_fs; //fieldsets
-  var left, opacity, scale; //fieldset properties which we will animate
-  var animating; //flag to prevent quick multi-click glitches
-
-  $(".next").click(function () {
-    if (animating) return false;
-    animating = true;
-
-    current_fs = $(this).parent();
-    next_fs = $(this).parent().next();
-
-    //activate next step on progressbar using the index of next_fs
-    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-    //show the next fieldset
-    next_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate({ opacity: 0 }, {
-      step: function (now, mx) {
-        //as the opacity of current_fs reduces to 0 - stored in "now"
-        //1. scale current_fs down to 80%
-        scale = 1 - (1 - now) * 0.2;
-        //2. bring next_fs from the right(50%)
-        left = (now * 50) + "%";
-        //3. increase opacity of next_fs to 1 as it moves in
-        opacity = 1 - now;
-        current_fs.css({
-          'transform': 'scale(' + scale + ')',
-          'position': 'absolute'
-        });
-        next_fs.css({ 'left': left, 'opacity': opacity });
-      },
-      duration: 300,
-      complete: function () {
-        current_fs.hide();
-        animating = false;
-      },
-      //this comes from the custom easing plugin
-      easing: 'easeInOutBack'
-    });
-  });
-
-  $(".previous").click(function () {
-    if (animating) return false;
-    animating = true;
-
-    current_fs = $(this).parent();
-    previous_fs = $(this).parent().prev();
-
-    //de-activate current step on progressbar
-    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-    //show the previous fieldset
-    previous_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate({ opacity: 0 }, {
-      step: function (now, mx) {
-        //as the opacity of current_fs reduces to 0 - stored in "now"
-        //1. scale previous_fs from 80% to 100%
-        scale = 0.8 + (1 - now) * 0.2;
-        //2. take current_fs to the right(50%) - from 0%
-        left = ((1 - now) * 50) + "%";
-        //3. increase opacity of previous_fs to 1 as it moves in
-        opacity = 1 - now;
-        current_fs.css({ 'left': left });
-        previous_fs.css({ 'transform': 'scale(' + scale + ')', 'opacity': opacity });
-      },
-      duration: 300,
-      complete: function () {
-        current_fs.hide();
-        animating = false;
-      },
-      //this comes from the custom easing plugin
-      easing: 'easeInOutBack'
-    });
-  });
 
 
 //=====Brand Image Slider=====//
@@ -264,11 +186,6 @@ $(document).ready(function () {
 
   });
  
-  $(".phone-call").click(function () {
-    $(".hide-number").css("display", "none");
-    $(".show-number").css("display", "inline");
-  });
-
 
   /*Featured Car Tabs */
   $('.tabess').scrollingTabs();
@@ -304,40 +221,6 @@ $(document).ready(function () {
   });
 
 
-  //========These JS code for the Dahsboard Profile Tab=========//
-  $(".name").click(function () {
-    var name_input = document.querySelector('#user-name').value;
-    document.querySelector('.update-name').innerHTML = name_input;
-
-  });
-
-  $(".email").click(function () {
-    var email_input = document.querySelector('#user-email').value;
-    document.querySelector('.update-email').innerHTML = email_input;
-
-  });
-
-  $(".mobile").click(function () {
-    var mobile_input = document.querySelector('#mobile_number').value;
-    document.querySelector('.update-number').innerHTML = mobile_input;
-
-  });
-
-
-  $(".bio").click(function () {
-    var date_input = document.querySelector('#user-bio').value;
-    document.querySelector('.update-bio').innerHTML = date_input;
-
-  });
-
-
-  $(".address").click(function () {
-    var street_input = document.querySelector('#billing_city').value;
-    document.querySelector('.street-update').innerHTML = street_input;
-
-    var city_input = document.querySelector('#billing_street').value;
-    document.querySelector('.city-update').innerHTML = city_input;
-  });
 
 
 
@@ -378,6 +261,84 @@ $(document).ready(function () {
   });
 
 
+  // https://www.linkedin.com/in/atakangk/
+  //jQuery time
+  var current_fs, next_fs, previous_fs; //fieldsets
+  var left, opacity, scale; //fieldset properties which we will animate
+  var animating; //flag to prevent quick multi-click glitches
+
+  $(".next").click(function () {
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    //activate next step on progressbar using the index of next_fs
+    // $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({ opacity: 0 }, {
+      step: function (now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale current_fs down to 80%
+        scale = 1 - (1 - now) * 0.2;
+        //2. bring next_fs from the right(50%)
+        left = (now * 50) + "%";
+        //3. increase opacity of next_fs to 1 as it moves in
+        opacity = 1 - now;
+        current_fs.css({
+          'transform': 'scale(' + scale + ')',
+          'position': 'absolute'
+        });
+        next_fs.css({ 'left': left, 'opacity': opacity });
+      },
+      duration: 200,
+      complete: function () {
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: 'easeInOutBack'
+    });
+  });
+
+  $(".previous").click(function () {
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+
+    //de-activate current step on progressbar
+    //  $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+    //show the previous fieldset
+    previous_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({ opacity: 0 }, {
+      step: function (now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale previous_fs from 80% to 100%
+        scale = 0.8 + (1 - now) * 0.2;
+        //2. take current_fs to the right(50%) - from 0%
+        left = ((1 - now) * 50) + "%";
+        //3. increase opacity of previous_fs to 1 as it moves in
+        opacity = 1 - now;
+        current_fs.css({ 'left': left });
+        previous_fs.css({ 'transform': 'scale(' + scale + ')', 'opacity': opacity });
+      },
+      duration: 200,
+      complete: function () {
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: 'easeInOutBack'
+    });
+  });
 
   //Internation Phone Code//
   let phone_number = document.querySelector("#mobile_number");
@@ -396,6 +357,7 @@ $(document).ready(function () {
     //onlyCountries: ["ae", "bh", "kw", "qa", "sa", "bd"],
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@19.2.16/build/js/utils.js"
   });
+
 
   
   //=======Search Result Tamplate FIlter========//
@@ -429,7 +391,49 @@ $(document).ready(function () {
 
 
 
-  
+  //========These JS code for the Dahsboard Profile Tab=========//
+  $(".name").click(function () {
+    var name_input = document.querySelector('#user-name').value;
+    document.querySelector('.update-name').innerHTML = name_input;
+
+  });
+
+  $(".email").click(function () {
+    var email_input = document.querySelector('#user-email').value;
+    document.querySelector('.update-email').innerHTML = email_input;
+
+  });
+
+  $(".mobile").click(function () {
+    var mobile_input = document.querySelector('#mobile_number').value;
+    document.querySelector('.update-number').innerHTML = mobile_input;
+
+  });
+
+
+  $(".bio").click(function () {
+    var date_input = document.querySelector('#user-bio').value;
+    document.querySelector('.update-bio').innerHTML = date_input;
+
+  });
+
+
+  $(".address").click(function () {
+    var street_input = document.querySelector('#billing_city').value;
+    document.querySelector('.street-update').innerHTML = street_input;
+
+    var city_input = document.querySelector('#billing_street').value;
+    document.querySelector('.city-update').innerHTML = city_input;
+  });
+
+
+
+  $(".phone-call").click(function () {
+    $(".hide-number").css("display", "none");
+    $(".show-number").css("display", "inline");
+  });
+
+
 
 
 //Car Details Slider Counter//
